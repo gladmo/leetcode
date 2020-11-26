f2237de6a25dadfee5c5051431882f3c84c01e3f
gladmo <mo@gladmo.com>
2020-11-26 23:07:38 +0800

    add question: 1013, fibonacci number

---
+++ questions/serial/简单/1013/golang/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/简单/1013/golang/solution"
+)
+
+func main() {
+
+	tests := []struct {
+		name  string
+		input int
+		want  int
+	}{
+		{
+			name:  "test-2-1",
+			input: 2,
+			want:  1,
+		},
+		{
+			name:  "test-3-2",
+			input: 3,
+			want:  2,
+		},
+		{
+			name:  "test-4-3",
+			input: 4,
+			want:  3,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		got := solution.Export(test.input)
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
