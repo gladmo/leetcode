5e9aac4af9f667131697b64510d0ff1f2e8bb970
gladmo <mo@gladmo.com>
2020-11-27 21:35:28 +0800

    [E:10/529, M:9/965, H:0/384] add No: 95 Unique Binary Search Trees II

---
+++ questions/tags/树/中等/unique-binary-search-trees-ii/golang/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/questions/tags/树/中等/unique-binary-search-trees-ii/golang/solution"
+	"github.com/gladmo/leetcode/leet"
+)
+
+func main() {
+	/*
+     
+	3
+
+    */
+
+	tests := []struct {
+		name  string
+		input [][]int
+		want  bool
+	}{
+		{
+			name: "test-[[1],[2],[3],[]]",
+			input: [][]int{
+				{1},
+				{2},
+				{3},
+				{},
+			},
+			want: true,
+		},
+		{
+			name: "test-[[1,3],[3,0,1],[2],[0]]",
+			input: [][]int{
+				{1, 3},
+				{3, 0, 1},
+				{2},
+				{0},
+			},
+			want: false,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		got := solution.Export(test.input)
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
---
+++ questions/tags/树/中等/unique-binary-search-trees-ii/golang/solution/unique-binary-search-trees-ii.go.clean
@@ -0,0 +1,29 @@
+package solution
+
+
+type TreeNode struct {
+     Val int
+     Left *TreeNode
+     Right *TreeNode
+}
+
+func Export(n int) []*TreeNode {
+	return generateTrees(n)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func generateTrees(n int) []*TreeNode {
+
+}
