d7d572121a64f57f22c4495868533f39a9404f31
gladmo <mo@gladmo.com>
2020-11-27 23:49:37 +0800

    [E:11/529, M:11/965, H:0/384] add No: 783 Search in a Binary Search Tree

---
+++ questions/serial/简单/783/golang/solution/search-in-a-binary-search-tree.go
@@ -0,0 +1,30 @@
+package solution
+
+// type TreeNode struct {
+// 	Val   int
+// 	Left  *TreeNode
+// 	Right *TreeNode
+// }
+
+type TreeNode = tree.Node
+
+func Export(root *TreeNode, val int) *TreeNode {
+	return searchBST(root, val)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+
+}
