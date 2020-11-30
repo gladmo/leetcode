629bdf94860098c93cd792749a8d0aeee141aa70
gladmo <mo@gladmo.com>
2020-11-30 16:51:12 +0800

    [E:21/531, M:15/969, H:0/386] add No: 33 Search in Rotated Sorted Array

---
+++ questions/tags/二分查找/中等/search-in-rotated-sorted-array/golang/solution/search-in-rotated-sorted-array.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(nums []int, target int) int {
+	return search(nums, target)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func search(nums []int, target int) int {
+
+}
