ebc60c6d0adba71876cf2f04cd67e68773662269
gladmo <mo@gladmo.com>
2020-12-02 00:33:04 +0800

    [E:28/531, M:19/969, H:0/386] add No: 349 Intersection of Two Arrays

---
+++ questions/serial/简单/349/golang/solution/intersection-of-two-arrays.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(nums1 []int, nums2 []int) []int {
+	return intersection(nums1, nums2)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func intersection(nums1 []int, nums2 []int) []int {
+
+}
