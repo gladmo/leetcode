249421cb8e809e985b03c4e61c3ec8cd73eb3412
gladmo <mo@gladmo.com>
2020-12-01 22:02:50 +0800

    [E:22/531, M:19/969, H:0/386] add No: 658 Find K Closest Elements

---
+++ questions/serial/中等/658/golang/solution/find-k-closest-elements.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(arr []int, k int, x int) []int {
+	return findClosestElements(arr, k, x)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func findClosestElements(arr []int, k int, x int) []int {
+
+}
