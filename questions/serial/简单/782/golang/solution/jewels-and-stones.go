1a79cbe2c6efe78b8ca65c9802d88f882861e36d
gladmo <mo@gladmo.com>
2020-12-03 00:55:40 +0800

    [E:36/531, M:20/969, H:1/386] add No: 782 Jewels and Stones

---
+++ questions/serial/简单/782/golang/solution/jewels-and-stones.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(J string, S string) int {
+	return numJewelsInStones(J, S)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func numJewelsInStones(J string, S string) int {
+
+}
