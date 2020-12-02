e8b202db650e4f704b3e2af6bf9540e4a80e8285
gladmo <mo@gladmo.com>
2020-12-02 22:31:58 +0800

    [E:34/531, M:20/969, H:1/386] add No: 219 Contains Duplicate II

---
+++ questions/serial/简单/219/golang/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/简单/219/golang/solution"
+)
+
+func main() {
+
+	tests := []struct {
+		name   string
+		input1 []int
+		input2 int
+		want   bool
+	}{
+		{
+			name:   "test-[1,2,3,1]-3",
+			input1: []int{1, 2, 3, 1},
+			input2: 3,
+			want:   true,
+		},
+		{
+			name:   "test-[1,0,1,1]-1",
+			input1: []int{1, 0, 1, 1},
+			input2: 1,
+			want:   true,
+		},
+		{
+			name:   "test-[1,2,3,1,2,3]-2",
+			input1: []int{1, 2, 3, 1, 2, 3},
+			input2: 2,
+			want:   false,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			solution.Export(test.input1, test.input2)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		got := solution.Export(test.input1, test.input2)
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
---
+++ questions/tags/数组/简单/contains-duplicate-ii/golang/solution/contains-duplicate-ii.go.clean
@@ -0,0 +1,14 @@
+package solution
+
+func Export(nums []int, k int) bool {
+	return containsNearbyDuplicate(nums, k)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+
+}
