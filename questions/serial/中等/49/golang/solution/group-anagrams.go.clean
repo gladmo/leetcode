a6e18483cff24d4082150628d48eabd8d0f29a68
gladmo <mo@gladmo.com>
2020-12-02 22:55:17 +0800

    [E:35/531, M:20/969, H:1/386] add No: 49 Group Anagrams

---
+++ questions/serial/中等/49/golang/solution/group-anagrams.go.clean
@@ -0,0 +1,14 @@
+package solution
+
+func Export(strs []string) [][]string {
+	return groupAnagrams(strs)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func groupAnagrams(strs []string) [][]string {
+
+}
---
+++ questions/tags/哈希表/中等/group-anagrams/golang/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"sort"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/中等/49/golang/solution"
+)
+
+func main() {
+	/*
+
+		["eat","tea","tan","ate","nat","bat"]
+
+	*/
+
+	tests := []struct {
+		name  string
+		input []string
+		want  [][]string
+	}{
+		{
+			name:  `test-["eat","tea","tan","ate","nat","bat"]`,
+			input: []string{"eat", "tea", "tan", "ate", "nat", "bat"},
+			want: [][]string{
+				{"ate", "eat", "tea"},
+				{"nat", "tan"},
+				{"bat"},
+			},
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		got := solution.Export(test.input)
+		for _, strs := range got {
+			sort.Strings(strs)
+		}
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
