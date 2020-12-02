0328dce8a51c2d65a054d650121c377f565a973e
gladmo <mo@gladmo.com>
2020-12-02 21:55:52 +0800

    [E:33/531, M:20/969, H:1/386] add No: 321 Create Maximum Number

---
+++ questions/serial/困难/321/golang/solution/create-maximum-number.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(nums1 []int, nums2 []int, k int) []int {
+	return maxNumber(nums1, nums2, k)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func maxNumber(nums1 []int, nums2 []int, k int) []int {
+
+}
