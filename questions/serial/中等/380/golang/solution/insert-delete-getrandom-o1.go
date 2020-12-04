8095dc379ec962d907a7360c1c44989db468d213
gladmo <mo@gladmo.com>
2020-12-04 13:00:42 +0800

    [E:37/531, M:24/969, H:1/386] add No: 380 Insert Delete GetRandom O(1)

---
+++ questions/serial/中等/380/golang/solution/insert-delete-getrandom-o1.go
@@ -0,0 +1,43 @@
+package solution
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+type RandomizedSet struct {
+
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() RandomizedSet {
+
+}
+
+
+/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+func (this *RandomizedSet) Insert(val int) bool {
+
+}
+
+
+/** Removes a value from the set. Returns true if the set contained the specified element. */
+func (this *RandomizedSet) Remove(val int) bool {
+
+}
+
+
+/** Get a random element from the set. */
+func (this *RandomizedSet) GetRandom() int {
+
+}
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
