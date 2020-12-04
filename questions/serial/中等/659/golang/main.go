dac2aa924ef34a16f22a92693312212e369892f6
gladmo <mo@gladmo.com>
2020-12-04 17:58:40 +0800

    [E:38/531, M:26/969, H:1/386] add No: 659 Split Array into Consecutive Subsequences

---
+++ questions/serial/中等/659/golang/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/中等/659/golang/solution"
+)
+
+func main() {
+
+	tests := []struct {
+		name  string
+		input []int
+		want  bool
+	}{
+		{
+			name:  "test-[1,2,3,3,4,4,5,5]",
+			input: []int{1, 2, 3, 3, 4, 4, 5, 5},
+			want:  true,
+		},
+		{
+			name:  "test-[1,2,3,3,4,5]",
+			input: []int{1, 2, 3, 3, 4, 5},
+			want:  true,
+		},
+		{
+			name:  "test-[1,2,3,4,4,5]",
+			input: []int{1, 2, 3, 4, 4, 5},
+			want:  false,
+		},
+		{
+			name:  "test-[1,2,3,3,3,4,4,5]",
+			input: []int{1, 2, 3, 3, 3, 4, 4, 5},
+			want:  false,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		got := solution.Export(test.input)
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
