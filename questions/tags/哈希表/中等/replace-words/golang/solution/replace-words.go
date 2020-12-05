ab83cbfab939439adace057a8b2ecae28e8c5f61
gladmo <mo@gladmo.com>
2020-12-05 18:02:37 +0800

    [E:40/531, M:32/969, H:1/386] add No: 648 Replace Words

---
+++ questions/tags/哈希表/中等/replace-words/golang/solution/replace-words.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(dictionary []string, sentence string) string {
+	return replaceWords(dictionary, sentence)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func replaceWords(dictionary []string, sentence string) string {
+
+}
