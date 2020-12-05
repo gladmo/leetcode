23ee90cb9164627bb720feff317e81199f6483ea
gladmo <mo@gladmo.com>
2020-12-05 16:39:43 +0800

    [E:40/531, M:30/969, H:1/386] add No: 208 Implement Trie (Prefix Tree)

---
+++ questions/serial/中等/208/golang/solution/implement-trie-prefix-tree.go
@@ -0,0 +1,43 @@
+package solution
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+type Trie struct {
+
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() Trie {
+
+}
+
+
+/** Inserts a word into the trie. */
+func (this *Trie) Insert(word string)  {
+
+}
+
+
+/** Returns if the word is in the trie. */
+func (this *Trie) Search(word string) bool {
+
+}
+
+
+/** Returns if there is any word in the trie that starts with the given prefix. */
+func (this *Trie) StartsWith(prefix string) bool {
+
+}
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
