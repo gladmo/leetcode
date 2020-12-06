477d8fc6a4e8f798abd72782043e773931c4bd4c
gladmo <mo@gladmo.com>
2020-12-06 23:57:33 +0800

    [E:44/531, M:41/969, H:2/386] add No: 384 Shuffle an Array

---
+++ questions/serial/中等/384/golang/solution/shuffle-an-array.go
@@ -0,0 +1,35 @@
+package solution
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+type Solution struct {
+
+}
+
+
+func Constructor(nums []int) Solution {
+
+}
+
+
+/** Resets the array to its original configuration and return it. */
+func (this *Solution) Reset() []int {
+
+}
+
+
+/** Returns a random shuffling of the array. */
+func (this *Solution) Shuffle() []int {
+
+}
+
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.Reset();
+ * param_2 := obj.Shuffle();
+ */
