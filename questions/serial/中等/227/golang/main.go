dfd905b3a7deeae1196c0e4e8346b8f654463b91
gladmo <mo@gladmo.com>
2020-12-07 23:07:30 +0800

    [E:44/531, M:46/969, H:5/386] add No: 227 Basic Calculator II

---
+++ questions/serial/中等/227/golang/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/中等/227/golang/solution"
+)
+
+func main() {
+
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "test-1*2-3/4+5*6-7*8+9/10",
+			input: "1*2-3/4+5*6-7*8+9/10",
+			want:  -24,
+		},
+		{
+			name:  "test-3+2*2",
+			input: "3+2*2",
+			want:  7,
+		},
+		{
+			name:  "test-3/2",
+			input: "3/2",
+			want:  1,
+		},
+		{
+			name:  "test-3/2",
+			input: " 3/2 ",
+			want:  1,
+		},
+		{
+			name:  "test- 3+5 / 2 ",
+			input: " 3+5 / 2 ",
+			want:  5,
+		},
+		{
+			name:  "test- ... ",
+			input: "3+5 / 2 + 8   + 2 * 9/3 + 3 * 5 + 2 + 5 -12 + 3 * 5 * 5 +2 * 5 +10 -23 -3 -5 + 10 / 3",
+			want:  96,
+		},
+		{
+			name:  "test- ... ",
+			input: "5 -12 +2 * 5 +10 -2",
+			want:  11,
+		},
+		{
+			name:  "test-4/3+2",
+			input: "4/3+2",
+			want:  3,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		got := solution.Export(test.input)
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
---
+++ questions/serial/中等/227/golang/solution/basic-calculator-ii.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(s string) int {
+	return calculate(s)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func calculate(s string) int {
+
+}
