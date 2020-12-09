1a2da525948a5ef40ad9d58fd294508cd58b90db
gladmo <mo@gladmo.com>
2020-12-09 13:36:40 +0800

    [E:46/531, M:52/969, H:6/386] add No: 191 Number of 1 Bits

---
+++ questions/tags/位运算/简单/number-of-1-bits/golang/solution/number-of-1-bits.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(num uint32) int {
+	return hammingWeight(num)    
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func hammingWeight(num uint32) int {
+    
+}
