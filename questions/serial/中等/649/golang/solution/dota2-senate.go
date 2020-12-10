49de4f6deae99f33855614b127bff4d79172f094
gladmo <mo@gladmo.com>
2020-12-11 00:44:19 +0800

    [E:56/531, M:53/969, H:6/386] add No: 649 Dota2 Senate

---
+++ questions/serial/中等/649/golang/solution/dota2-senate.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(senate string) string {
+	return predictPartyVictory(senate)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func predictPartyVictory(senate string) string {
+
+}
