b103edcd0aa847d0078e04bf23aed21409d1b0ae
gladmo <mo@gladmo.com>
2020-12-10 22:09:48 +0800

    [E:53/531, M:52/969, H:6/386] add No: 412 Fizz Buzz

---
+++ questions/serial/简单/412/README.md
@@ -0,0 +1,33 @@
+## [Fizz Buzz](https://leetcode-cn.com/problems/fizz-buzz/)
+
+写一个程序，输出从 1 到 _n_ 数字的字符串表示。
+
+1. 如果 _n _是3的倍数，输出“Fizz”；
+
+2. 如果 _n _是5的倍数，输出“Buzz”；
+
+3.如果 _n _同时是3和5的倍数，输出 “FizzBuzz”。
+
+**示例：**
+
+`n = 15,
+
+返回:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+`
---
+++ questions/serial/简单/412/golang/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/简单/412/golang/solution"
+)
+
+func main() {
+
+	tests := []struct {
+		name  string
+		input int
+		want  []string
+	}{
+		{
+			name:  "test-1",
+			input: 1,
+			want:  []string{"1"},
+		},
+		{
+			name:  "test-15",
+			input: 15,
+			want: []string{
+				"1",
+				"2",
+				"Fizz",
+				"4",
+				"Buzz",
+				"Fizz",
+				"7",
+				"8",
+				"Fizz",
+				"Buzz",
+				"11",
+				"Fizz",
+				"13",
+				"14",
+				"FizzBuzz",
+			},
+		},
+		{
+			name:  "test-100",
+			input: 100,
+			want:  []string{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz", "31", "32", "Fizz", "34", "Buzz", "Fizz", "37", "38", "Fizz", "Buzz", "41", "Fizz", "43", "44", "FizzBuzz", "46", "47", "Fizz", "49", "Buzz", "Fizz", "52", "53", "Fizz", "Buzz", "56", "Fizz", "58", "59", "FizzBuzz", "61", "62", "Fizz", "64", "Buzz", "Fizz", "67", "68", "Fizz", "Buzz", "71", "Fizz", "73", "74", "FizzBuzz", "76", "77", "Fizz", "79", "Buzz", "Fizz", "82", "83", "Fizz", "Buzz", "86", "Fizz", "88", "89", "FizzBuzz", "91", "92", "Fizz", "94", "Buzz", "Fizz", "97", "98", "Fizz", "Buzz"},
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		got := solution.Export(test.input)
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
