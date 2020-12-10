2b9d9c778e9f87ee152ee74c0f91cf35502ae2b4
gladmo <mo@gladmo.com>
2020-12-10 20:54:31 +0800

    [E:52/531, M:52/969, H:6/386] add No: 121 Best Time to Buy and Sell Stock

---
+++ questions/tags/动态规划/简单/best-time-to-buy-and-sell-stock/golang/solution/best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,14 @@
+package solution
+
+func Export(prices []int) int {
+	return maxProfit(prices)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func maxProfit(prices []int) int {
+
+}
