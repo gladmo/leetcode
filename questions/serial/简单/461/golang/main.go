0ccf8be2bd7df0a4c2969e6efb69e0196c76d7f4
gladmo <mo@gladmo.com>
2020-12-10 23:39:15 +0800

    [E:56/531, M:52/969, H:6/386] add No: 461 Hamming Distance

---
+++ questions/serial/简单/461/golang/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/简单/461/golang/solution"
+)
+
+func main() {
+
+	tests := []struct {
+		name   string
+		input1 int
+		input2 int
+		want   int
+	}{
+		{
+			name:   "test-1-4",
+			input1: 1,
+			input2: 4,
+			want:   2,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			solution.Export(test.input1, test.input2)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		got := solution.Export(test.input1, test.input2)
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
---
+++ questions/serial/简单/461/golang/solution/hamming-distance.go.clean
@@ -0,0 +1,14 @@
+package solution
+
+func Export(x int, y int) int {
+	return hammingDistance(x, y)
+}
+
+/****************************************************/
+/******** 以下为 Leetcode 示例部分（提交PR请还原） *******/
+/******** 使用 (./leetcode clear) 初始化所有问题 *******/
+/****************************************************/
+
+func hammingDistance(x int, y int) int {
+
+}
