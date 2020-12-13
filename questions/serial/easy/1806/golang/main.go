acb8b32655f2fa2f7c482f6882cfe6cd1c539456
gladmo <mo@gladmo.com>
2020-12-13 17:36:22 +0800

    [E:64/533, M:57/974, H:6/387] add No: 1806 Count of Matches in Tournament

---
+++ questions/serial/easy/1806/golang/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/easy/1806/golang/solution"
+)
+
+func main() {
+
+	tests := []struct {
+		name  string
+		input int
+		want  int
+	}{
+		{
+			name:  "test-7",
+			input: 7,
+			want:  6,
+		},
+		{
+			name:  "test-14",
+			input: 14,
+			want:  13,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		got := test.want
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			got = solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
