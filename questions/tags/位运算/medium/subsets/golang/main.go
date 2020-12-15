501bf84c5d83d8f04d580961ea07ad642d5b64f5
gladmo <mo@gladmo.com>
2020-12-16 00:49:49 +0800

    [E:69/533, M:69/974, H:6/387] add No: 78: Subsets

---
+++ questions/tags/位运算/medium/subsets/golang/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/questions/tags/位运算/medium/subsets/golang/solution"
+	"github.com/gladmo/leetcode/leet"
+)
+
+func main() {
+	/*
+     
+	[1,2,3]
+
+    */
+
+	tests := []struct {
+		name  string
+		input [][]int
+		want  bool
+	}{
+		{
+			name: "test-[[1],[2],[3],[]]",
+			input: [][]int{
+				{1},
+				{2},
+				{3},
+				{},
+			},
+			want: true,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		got := test.want
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			got = solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
