defeb673475942a8719752f35334eff5563b2442
gladmo <mo@gladmo.com>
2020-12-22 19:52:08 +0800

    [E:76/537, M:71/980, H:9/391] add No: 1086: Divisor Game

---
+++ questions/serial/easy/1086/golang/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/gladmo/leetcode/leet"
+	"github.com/gladmo/leetcode/questions/serial/easy/1086/golang/solution"
+)
+
+func main() {
+
+	tests := []struct {
+		name  string
+		input int
+		want  bool
+	}{
+		{
+			name:  "test-2",
+			input: 2,
+			want:  true,
+		},
+		{
+			name:  "test-3",
+			input: 3,
+			want:  false,
+		},
+	}
+
+	testLog := leet.NewTestLog(len(tests))
+	defer testLog.Render()
+
+	timeoutDuration := time.Second * 2
+
+	for idx, test := range tests {
+		// 超时检测
+		got := test.want
+		timeout := leet.Timeout(timeoutDuration, func(ctx context.Context, cancel context.CancelFunc) {
+			got = solution.Export(test.input)
+			cancel()
+		})
+
+		if timeout {
+			testLog.Fail(idx+1, test.name, "timeout")
+			continue
+		}
+
+		if !reflect.DeepEqual(test.want, got) {
+			testLog.Fail(idx+1, test.name, fmt.Sprintf("want: %v, got %v.", test.want, got))
+			continue
+		}
+
+		testLog.Pass(idx+1, test.name)
+	}
+}
